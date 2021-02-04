100375f585ed9d45e94a6275679012241c8f055b
Pranay-kumar-123 <vorugantipranay@gmail.com>
2021-02-04 20:53:47 +0530

    nearbydoctor

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ app.get('/', function(req, res){
   app.get('/doctor/login', function(req, res){
     res.sendFile(__dirname+'/frontend/html/doctorlogin.html');
   });
+  app.get('/doctor/register', function(req, res){
+    res.sendFile(__dirname+'/frontend/html/doctorregister.html');
+  });
+  app.get('/patient/login', function(req, res){
+    res.sendFile(__dirname+'/frontend/html/patientlogin.html');
+  });
+  app.get('/patient/register', function(req, res){
+    res.sendFile(__dirname+'/frontend/html/patientregister.html');
+  });
+  app.get('/aboutus', function(req, res){
+    res.sendFile(__dirname+'/frontend/html/aboutus.html');
+  });
   app.listen(port, function(){
     console.log("Site Running on http://localhost:"+port);
 });
