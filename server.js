8db16167753598da83c214bd765b30efcc8086f0
Pranay-kumar-123 <vorugantipranay@gmail.com>
2021-02-04 20:55:47 +0530

    done

--- server.js
+++ server.js
@@ -1,5 +1,60 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://HAnWNJS3Mj7U5oem:pedhu@cluster0.flgd8.mongodb.net/nearbydoctor?retryWrites=true&w=majority', {});
+mongoose.connection.on('open', function(){
+    console.log("MongoDB Connected ");
+})
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+var userdschema=mongoose.Schema(
+    {
+      username:String,
+      email:String,
+      password:String,
+      specialist:String,
+      hname:String,
+      hadrs:String,
+      time:String,
+      cfee:String
+    }
+  );
+  var userdmodel=mongoose.model("doctor", userdschema);
+  app.post("/api/dctrregister", (req,res)=>{
+    var userd=userdmodel(req.body);
+    userd.save();
+    res.redirect("/doctor/login");
+  })
+  app.post("/api/dctrlogin", (req,res)=>{
+    userdmodel.find(req.body,(err,userddetails)=>{
+      if(userddetails.length>0)
+      res.redirect("/success");
+      else res.redirect("/error");
+    })
+    //res.redirect("/");
+  })
+  var userpschema=mongoose.Schema(
+    {
+      username:String,
+      email:String,
+      password:String,
+      gender:String
+    }
+  );
+  var userpmodel=mongoose.model("patient", userpschema);
+  app.post("/api/patientregister", (req,res)=>{
+    var userp=userpmodel(req.body);
+    userp.save();
+    res.redirect("/patient/login");
+  })
+  app.post("/api/patientlogin", (req,res)=>{
+    userpmodel.find(req.body,(err,userpdetails)=>{
+      if(userpdetails.length>0)
+      res.redirect("/success");
+      else res.redirect("/error");
+    })
+  })
 
 app.use(express.static(__dirname+'/frontend'));
 var port= process.env.PORT  || 3000;
